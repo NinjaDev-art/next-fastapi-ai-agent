d89df0a64f3fbe45635a17fee6bbce8695fa0d76
LedgerNinja0x <sweetheart199558@gmail.com>
2025-05-02 15:35:18 -0700

    fix: chat history

--- app/models/chat.py
+++ app/models/chat.py
@@ -4,14 +4,7 @@ from datetime import datetime
 
 class IRouterChatLog(BaseModel):
     prompt: str
-    model: str
-    inputToken: int
-    outputToken: int
-    points: int
     response: Optional[str] = None
-    timestamp: Optional[datetime] = None
-    outputTime: int
-    fileUrls: List[str]
 
 class ChatRequest(BaseModel):
     prompt: str
